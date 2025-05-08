9c7743e2d8c162d00fb1f813571776f204545096
lmatos01 <44268606+lmatos01@users.noreply.github.com>
2025-05-08 19:04:28 -0400

    Update config.mjs

--- config.mjs
+++ config.mjs
@@ -1,4 +1,4 @@
-export const WEBSOCKET_PUBLIC_URL = "http://localhost:5505/"
+export const WEBSOCKET_PUBLIC_URL = "http://34.173.64.212:5505/"
 export const WEBSOCKET_SERVER_PORT = 5505
 
 const QUIZZ_CONFIG = {
